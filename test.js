d920f3274bd15cf0183f0a625802210e769a1d15
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-09-06 20:45:07 +0200

    Use AVA for testing

--- test.js
+++ test.js
@@ -1,26 +1,30 @@
 'use strict';
-var assert = require('assert');
+var test = require('ava');
 var quarter = require('./');
 
-it('should get the quarter of the year', function () {
-	assert.equal(quarter(new Date('January 1, 2000')), 1);
-	assert.equal(quarter(new Date('February 1, 2000')), 1);
-	assert.equal(quarter(new Date('March 1, 2000')), 1);
+test('should get the quarter of the year', function (t) {
+	t.is(quarter(new Date('January 1, 2000')), 1);
+	t.is(quarter(new Date('February 1, 2000')), 1);
+	t.is(quarter(new Date('March 1, 2000')), 1);
 
-	assert.equal(quarter(new Date('April 1, 2015')), 2);
-	assert.equal(quarter(new Date('May 1, 2015')), 2);
-	assert.equal(quarter(new Date('June 1, 2015')), 2);
+	t.is(quarter(new Date('April 1, 2015')), 2);
+	t.is(quarter(new Date('May 1, 2015')), 2);
+	t.is(quarter(new Date('June 1, 2015')), 2);
 
-	assert.equal(quarter(new Date('July 20, 1950')), 3);
-	assert.equal(quarter(new Date('August 20, 1950')), 3);
-	assert.equal(quarter(new Date('September 20, 1950')), 3);
+	t.is(quarter(new Date('July 20, 1950')), 3);
+	t.is(quarter(new Date('August 20, 1950')), 3);
+	t.is(quarter(new Date('September 20, 1950')), 3);
 
-	assert.equal(quarter(new Date('October 3, 2050')), 4);
-	assert.equal(quarter(new Date('November 3, 2050')), 4);
-	assert.equal(quarter(new Date('December 3, 2050')), 4);
+	t.is(quarter(new Date('October 3, 2050')), 4);
+	t.is(quarter(new Date('November 3, 2050')), 4);
+	t.is(quarter(new Date('December 3, 2050')), 4);
+
+	t.end();
 });
 
-it('should make the `date` argument default to the current time', function () {
+test('should make the `date` argument default to the current time', function (t) {
 	var q = quarter();
-	assert(q === 1 || q === 2 || q === 3 || q === 4);
+	t.true(q === 1 || q === 2 || q === 3 || q === 4);
+
+	t.end();
 });
